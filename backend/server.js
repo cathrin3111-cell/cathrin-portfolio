6d589eec3b6461a543755bdba39b64e474cd4984
cathrin3111-cell <cathrin3111@gmail.com>
2026-03-27 13:59:47 +0530

    Fix pool variable name

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('./db');
+const pool = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
