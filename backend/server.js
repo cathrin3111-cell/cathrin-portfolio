eac48d02f9cf3dead5d8458b2349da17efe5a324
cathrin3111-cell <cathrin3111@gmail.com>
2026-03-27 11:39:15 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://cathrin.github.io'
+    origin: 'https://cathrin3111-cell.github.io'
 }));
 app.use(express.json());
 
